6472d30bb290b232b2029c813e6f586e801aa936
patrick03524 <dibujanteanime024@gmail.com>
2016-11-11 13:38:58 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ostream>
+#include <algorithm>
+#include "Pila.h"
+#include <iterator>
+#include <queue>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+main()
+{
+  cout<<"Allahu Akbar >:v"<<endl;
+}
